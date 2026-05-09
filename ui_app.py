3a06b577e1045777bf78bf85cec2b389f93a2861
Manish Verma <manisverma@gmail.com>
2026-05-09 17:31:32 +0530

    Logo 120px width

--- ui_app.py
+++ ui_app.py
@@ -10,7 +10,10 @@ from dotenv import load_dotenv
 
 st.set_page_config(page_title="Manver IQ", page_icon="📈", layout="wide")
 
-st.image("manver_logo.png", use_container_width=True)
+col_logo, _ = st.columns([1, 3])
+with col_logo:
+    st.image("manver_logo.png", width=120)
+st.caption("📈 Smart Stock Analysis | NSE/BSE")
 st.caption("📈 Smart Stock Analysis | NSE/BSE")
 
 st.markdown("""
